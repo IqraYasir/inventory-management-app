433ea91cf7cc95bf6d4de4cf2b4b5cedb86a4d66
IqraYasir <iqrayasir0906@gmail.com>
2024-05-06 18:29:41 +0100

    Add create category function

---
+++ populatedb.js
@@ -0,0 +1,33 @@
+const userArgs = process.argv.slice(2);
+
+const Category = require('./models/category');
+const Item = require('./models/item');
+
+const categories = [];
+const items = [];
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const mongoDB = userArgs[0];
+
+main().catch(err => console.log(err));
+
+async function main() {
+    console.log('Debug: About to connect');
+    await mongoose.connect(mongoDB);
+    console.log('Debug: Should be connected');
+    await createCategories();
+    await createItems();
+    console.log('Debug: Closing mongoose');
+    mongoose.connection.close();
+};
+
+async function createCategory(index, name, description) {
+    const categorydetail = {name: name, description: description};
+    const category = new Category(categorydetail);
+
+    await category.save();
+    categories[index] = category;
+    console.log(`Added category: ${name}`);
+};
