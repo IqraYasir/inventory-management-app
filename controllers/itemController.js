04422fbbac1c4394581db57977373fdff9abb0b1
IqraYasir <iqrayasir0906@gmail.com>
2024-05-09 19:08:38 +0100

    Fix syntax errors

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,11 +2,11 @@ const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
 
 exports.index = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Site Home Page");
+    res.send('NOT IMPLEMENTED: Site Home Page');
 });
   
 exports.item_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item list");
+    res.send('NOT IMPLEMENTED: Item list');
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
@@ -14,25 +14,25 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item create GET");
+    res.send('NOT IMPLEMENTED: Item create GET');
 });
 
 exports.item_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item create POST");
+    res.send('NOT IMPLEMENTED: Item create POST');
 });
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item delete GET");
+    res.send('NOT IMPLEMENTED: Item delete GET');
 });
 
 exports.item_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item delete POST");
+    res.send('NOT IMPLEMENTED: Item delete POST');
 });
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update GET");
+    res.send('NOT IMPLEMENTED: Item update GET');
 });
 
 exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update POST");
+    res.send('NOT IMPLEMENTED: Item update POST');
 });
