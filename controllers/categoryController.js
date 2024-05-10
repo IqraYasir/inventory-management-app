fdb61d56cf74a2b6c8ad3680de3efb665d21dd85
IqraYasir <iqrayasir0906@gmail.com>
2024-05-10 20:30:40 +0100

    Create controller and view for category detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require('../models/category');
+const Item =  require('../models/item');
 const asyncHandler = require('express-async-handler');
 
 exports.category_list = asyncHandler(async(req, res, next) => {
@@ -14,7 +15,22 @@ exports.category_list = asyncHandler(async(req, res, next) => {
 });
 
 exports.category_detail = asyncHandler(async(req, res,  next) => {
-    res.send(`NOT IMPLEMENTED: Category detail: ${req.params.id}`);
+    const [category, allItemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({category: req.params.id}).exec()
+    ]);
+
+    if (category === null) {
+        const err = new Error('Category not found');
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render('category_detail', {
+        title: 'Category Detail',
+        category: category,
+        category_items: allItemsInCategory
+    });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
